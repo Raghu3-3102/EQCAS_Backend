4c4472f33afea06b61fef616f8db15e1595d8f7a
Raghu3-3102 <raghvendrakumarpandey321@gmail.com>
2025-10-17 13:00:46 +0530

    Fix permission bug

--- src/routes/OverViewRoutes/OverViewRouter.js
+++ src/routes/OverViewRoutes/OverViewRouter.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get("/dashboard",authMiddleware,permissionMiddleware("overview"), getDashboardOverview);
 router.get("/dashboard/certifications/year/:year",authMiddleware,permissionMiddleware("overview"), getYearlyCertificationStats);
 router.get("/certifications/stats",authMiddleware,permissionMiddleware("overview"), getCertificationStatsByPeriod);
-router.get("/certifications/stats/monthly",authMiddleware.permissionMiddleware("overview"),getMonthlyCertificationStats)
+router.get("/certifications/stats/monthly",authMiddleware,permissionMiddleware("overview"),getMonthlyCertificationStats)
 router.get("/certifications/stats/today",authMiddleware, permissionMiddleware("overview"),getTodayCertificationStats )
 router.get("/compoy/regitration",authMiddleware,permissionMiddleware("overview"),getCompanyRegistrationsByMonth)
 router.get("/certifications/stats/weekly",authMiddleware,permissionMiddleware("overview"),getCurrentWeekCertificationStats)
